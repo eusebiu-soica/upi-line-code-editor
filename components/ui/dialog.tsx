2103dc9eca7a0763edd7a062a7d4345ca50bde76
eusebiu-soica <89060029+eusebiu-soica@users.noreply.github.com>
2026-01-08 21:44:54 +0200

    Add @radix-ui/react-dialog dependency and update dialog component import

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import * as React from "react"
-import * as DialogPrimitive from "@radix-ui/react-dialog"
+import { Dialog as DialogPrimitive } from "radix-ui"
 import { X } from "lucide-react"
 
 import { cn } from "@/lib/utils"
 
-const Dialog = DialogPrimitive.Root
+const Dialog = DialogPrimitive.Dialog
 
 const DialogTrigger = DialogPrimitive.Trigger
 
